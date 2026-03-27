fd46e44f0f86e1a2987dcf1814701114a398648f
gwenlli <gwen.li351@gmail.com>
2026-03-27 17:13:43 -0400

    Update batch.py

--- batch.py
+++ batch.py
@@ -15,4 +15,27 @@ class BatchedLoRA(torch.nn.Module):
         self.B = torch.nn.Parameter(torch.zeros(num_adapters, linear_layer.out_features, rank))
     
     def forward(self, x):
+        batch_size, seq_len, hidden_dim = x.shape
+        device = x.device
+
+        x = x.reshape(batch_size * seq_len, hidden_dim)
+
+        adapter_ids = torch.arange(batch_size, device=device) % self.num_adapters
+        adapter_ids = adapter_ids.repeat_interleave(seq_len)
+
+        # Inefficient - Change to group
+        A = self.A[adapter_ids]
+        B = self.B[adapter_ids]
+
+        W0x = self.linear_layer(x)
+
+        # Ax
+        Ax = torch.bmm(A, x.unsqueeze(-1)).squeeze(-1)
+        # BAx
+        BAx = torch.bmm(B, Ax.unsqueeze(-1)).squeeze(-1)
         
+        out = W0x + (self.alpha / self.rank) * BAx
+        out = out.reshape(batch_size, seq_len, -1)
+
+        return out
+
